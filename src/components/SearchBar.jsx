f3114395d4633bf8407fb8017eb30a8efbfd7400
Akash <adesigns>
2022-02-03 13:36:44 -0500

    second commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Paper, TextField, Typography} from "@material-ui/core";
-import "../styles/SearchBar.css";
 
 class SearchBar extends React.Component {
 
@@ -24,8 +23,9 @@ class SearchBar extends React.Component {
     render () {
 
         return (
-            <Paper elevation = {6} style = {{padding: "25px", backgroundColor: "#fff"}}>
-                <form onSubmit = {this.handleSubmit}>
+            <Paper className = "search-bar" elevation = {6} style = {{padding: "1vw", backgroundColor: "#333"}}>
+                <Typography variant = "h3" color = "secondary" style = {{fontWeight: "lighter", color: "#fff"}}> Custom Youtube App </Typography>
+                <form className = "form-input" onSubmit = {this.handleSubmit}>
                     <TextField fullWidth label = "Search A Video" onChange = {this.handleChange}/>
                 </form>
             </Paper>
